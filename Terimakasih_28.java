c11b033ae18d9445bbde4bcdaa228a96de490caa
vanessacristin <cristinvanessa6@gmail.com>
2023-11-23 11:25:48 +0700

    Jobsheet 12

---
+++ Terimakasih_28.java
@@ -0,0 +1,10 @@
+public class Terimakasih_28 {
+     public static void main(String[] args) {
+        UcapanTerimakasih();
+    }
+    public static void UcapanTerimakasih() {
+        System.out.println ("Thank you for being the best teacher in the world. \n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+
+    }
+    }
